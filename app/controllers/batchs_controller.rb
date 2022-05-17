41c7b6568052d4b0171615911b7f536fb60ef83c
Thibault Bayard <t.bayard.pro@gmail.com>
2022-05-17 16:50:13 +0200

    Controllers

--- app/controllers/batchs_controller.rb
+++ app/controllers/batchs_controller.rb
@@ -1,2 +1,38 @@
 class BatchsController < ApplicationController
+  def index
+    @batchs = Batch.all
+  end
+
+  def new
+    @batch = Batch.new
+  end
+
+  def create
+    @batch = Batch.new(params_batch)
+    if @batch.save
+      redirect_to(@batch)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @batch = Batch.find(params[:id])
+  end
+
+  def edit
+    @batch = Batch.find(params[:id])
+  end
+
+  def update
+    @batch = Batch.find(params[:id])
+    @batch.update(params_batch)
+    redirect_to batch_path(@batch)
+  end
+
+  private
+
+  def params_batch
+    params.require(:batch).permit(:batch_number)
+  end
 end
--- app/controllers/wagonners_controller.rb
+++ app/controllers/wagonners_controller.rb
@@ -1,2 +1,42 @@
 class WagonnersController < ApplicationController
+  def index
+    @wagonners = Wagonner.all
+  end
+
+  def new
+    @wagonner = Wagonner.new
+    @batch = Batch.find(params[:batchs_id])
+    @wagonner.batchs = @batch
+  end
+
+  def create
+    @wagonner = Wagonner.new(params_wagonner)
+    @batch = Batch.find(params[:batchs_id])
+    @wagonner.batchs = @batch
+    if @wagonner.save
+      redirect_to(@wagonner)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @wagonner = Wagonner.find(params[:id])
+  end
+
+  def edit
+    @wagonner = Wagonner.find(params[:id])
+  end
+
+  def update
+    @wagonner = Wagonner.find(params[:id])
+    @wagonner.update(params_wagonner)
+    redirect_to wagonner_path(@wagonner)
+  end
+
+  private
+
+  def params_wagonner
+    params.require(:wagonner).permit(:first_name, :last_name, :linkedin, :github, :email, :batchs_id)
+  end
 end
